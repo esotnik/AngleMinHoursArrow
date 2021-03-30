346dd9985aea724c61725d0f064526cccb690d77
Eve <e_sotnik@bk.ru>
2021-03-30 11:04:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AngleMinHoursArrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hours:");
+            string s = Console.ReadLine();
+            double h = double.Parse(s);
+            Console.WriteLine("Minutes:");
+            s = Console.ReadLine();
+            double m = double.Parse(s);
+            double angle = Math.Abs((h + m/60.0) * 30.0 - m * 6.0);
+            angle = Math.Min(360 - angle, angle);
+            Console.WriteLine("Angle is {0}", angle);
+        }
+    }
+}
